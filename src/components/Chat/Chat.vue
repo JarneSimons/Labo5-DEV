acf22c5f06bf5eb85a0a2f825b5e51c0d425db45
JarneSimons <r0901961@student.thomasmore.be>
2023-12-06 17:35:16 +0100

    feat: add possibility to add own comment

--- src/components/Chat/Chat.vue
+++ src/components/Chat/Chat.vue
@@ -15,32 +15,66 @@
         const response = await fetch("https://lab5-p379.onrender.com/api/v1/messages/");
         const data = await response.json();
         data.reverse();
-        allMessages.data = data.slice(0, 8);
+        allMessages.data = data.slice(0, 15);
     });
 
+    // send message to api https://lab5-p379.onrender.com/api/v1/messages/ and add a user and a message
+    const sendMessage = async () => {
+    try {
+        const response = await fetch("https://lab5-p379.onrender.com/api/v1/messages/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                user: 'Jarne',
+                text: message.value,
+
+            }),
+        });
+
+        console.log(response); // Log the entire response for debugging
+
+        const data = await response.json();
+
+        if (response.ok) {
+            console.log(data);
+            allMessages.data.push(data);
+            message.value = "";
+        } else {
+            console.error("Error saving message:", data.message);
+        }
+    } catch (error) {
+        console.error("Error:", error);
+    }
+};
 
    
 
 
-    const sendMessage = () => {
-        allMessages.data.push(message.value);
-        message.value = "";
-    }
-
 </script>
 
 <template>
-  <div>
-    <ul>
-        <li v-for="m in allMessages.data">{{ m }}</li>
-    </ul>
     <div>
+      <ul>
+        <li v-for="m in allMessages.data" :key="m._id" class="message-item">
+          <span class="username">{{ m.user }}:</span> {{ m.text }}
+        </li>
+      </ul>
+      <div>
         <input v-model="message" type="text" placeholder="" />
         <button @click="sendMessage">Send</button>
+      </div>
     </div>
-  </div>
-</template>
+  </template>
 
 <style scoped>
-  
+.message-item {
+  margin-bottom: 10px;
+}
+
+.username {
+  font-weight: bold;
+  margin-right: 5px;
+}
 </style>
